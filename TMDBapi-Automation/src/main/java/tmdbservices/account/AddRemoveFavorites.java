28deb75eb555efe7ab9605e9930dd8da2b0dfd67
berylsergious <berylsergious@gmail.com>
2025-07-09 22:40:12 +0530

    Added loggers and error handling sections

--- TMDBapi-Automation/src/main/java/tmdbservices/account/AddRemoveFavorites.java
+++ TMDBapi-Automation/src/main/java/tmdbservices/account/AddRemoveFavorites.java
@@ -8,16 +8,21 @@ import tmdbservices.auth.SessionManager;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class AddRemoveFavorites {
     
-	
+	private static final Logger logger = LogManager.getLogger(AddRemoveFavorites.class);
 	private static final String TEMPLATE_PATH = System.getProperty("user.dir") + "//src//test//resources//jasonPayload_Templates//Favorites.json";
 
     public static void addRemoveFavorites(String mediaType, int mediaId, boolean isFavorite) {
-       
+    	logger.info("Starting addRemoveFavorites operation - MediaType: {}, MediaID: {}, isFavorite: {}", 
+                mediaType, mediaId, isFavorite);
     	try {
     	// 1. Load Configurations
         String baseUri = ConfigManager.getConfigProperties("base.uri");
@@ -25,15 +30,20 @@ public class AddRemoveFavorites {
         String accountId = ConfigManager.getConfigProperties("account.id");
         String sessionId = SessionManager.getSessionId();
 
+        logger.debug("Configuration loaded - BaseURI: {}, AccountID: {}", baseUri, accountId);
+        
         // 2. Populate JSON Template
+        logger.debug("Populating JSON template from path: {}", TEMPLATE_PATH);
         Map<String, Object> templateValues = new HashMap<>();
         templateValues.put("media_type", mediaType);
         templateValues.put("media_id", mediaId);
         templateValues.put("favorite", isFavorite);
 
         String requestBody = JsonTemplateManager.populateTemplate(TEMPLATE_PATH, templateValues);
-
+        logger.debug("Request body populated: {}", requestBody);
+        
         // 3. Send API Request
+        logger.info("Sending API request to add/remove favorites");
         Response response = given()
             .baseUri(baseUri)
             .queryParam("api_key", apiKey)
@@ -42,15 +52,31 @@ public class AddRemoveFavorites {
             .body(requestBody)
             .when()
             .post("/account/" + accountId + "/favorite");
-
+        
+        // Log response details
+        logger.debug("API Response - Status: {}, Body: {}", 
+                     response.getStatusCode(), response.getBody().asString());
+        logger.info("API request completed in {} ms", response.getTime());
+             
         // 4. Assertions
-        response.then()
-            .statusCode(201)
-            .body("success", equalTo(true));
+        if (isFavorite== true) {  
+            response.then()
+                .statusCode(201)  // Addition case
+                .body("success", equalTo(true))
+                .body("status_code", equalTo(1));
+        } else {
+            response.then()
+                .statusCode(200)  // Deletion case
+                .body("success", equalTo(true))
+                .body("status_code", equalTo(13));
+        }
+        logger.info("Successfully completed addRemoveFavorites operation");
     
     } catch (IOException e) {
+    	logger.error("Failed to read JSON template from path: {}", TEMPLATE_PATH, e);
         throw new RuntimeException("Failed to read JSON template from path: " + TEMPLATE_PATH, e);
     } catch (Exception e) {
+    	logger.error("Error in addRemoveFavorites: {}", e.getMessage(), e);
         throw new RuntimeException("Error in addRemoveFavorites: " + e.getMessage(), e);
     }
 }
--- TMDBapi-Automation/src/test/java/testcases/accountApiTest/FavoritesTest.java
+++ TMDBapi-Automation/src/test/java/testcases/accountApiTest/FavoritesTest.java
@@ -2,6 +2,10 @@ package testcases.accountApiTest;
 
 import java.util.*;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -10,30 +14,53 @@ import tmdbservices.auth.AuthServices;
 import utils.ExcelTestDataManager;
 
 public class FavoritesTest {
-	
+	private static final Logger logger = LogManager.getLogger(FavoritesTest.class);
 	@DataProvider(name = "favoriteMovies")
 	public Object[][] getFavoriteMovies() throws Exception {
+		logger.info("Initializing test data provider for favorites");
+		
+		try {
+		logger.debug("Loading test data from Excel file");
 	    List<Map<String, Object>> sheetData = new ExcelTestDataManager("testdata/testdata.xlsx")
 	        .getSheetData("favorites"); // Worksheet name
 
 	    // Convert to TestNG's Object[][]
-	    return sheetData.stream()
+	    Object[][] testData =  sheetData.stream()
 	        .map(row -> new Object[]{
 	            row.get("media_type"), 
 	            row.get("media_id"), 
 	            row.get("favorite")
 	        })
 	        .toArray(Object[][]::new);
+		
+	    logger.debug("Successfully prepared test data provider");
+        return testData;
+        
+		}catch(Exception e) {
+            logger.error("Failed to initialize test data provider", e);
+            throw e;
+        }    
+	    
 	}
 
+	@BeforeClass
+    public void setup() {
+        logger.info("Initializing test session");
+        AuthServices.ensureLoggedIn();
+    }
+
+	
 	@Test(dataProvider = "favoriteMovies")
-	public void testFavorites(String mediaType, int mediaId, boolean favorite) {
-	    
-		AuthServices.ensureLoggedIn();
+	public void testFavorites(String mediaType, int mediaId, boolean favorite) {	
 		// Uses the same AddRemoveFavorites service
 	    AddRemoveFavorites.addRemoveFavorites(mediaType, mediaId, favorite);
-	    
-	    AuthServices.deleteSession();
+	}
+	
+	@AfterClass
+    public void teardown() {
+		
+		 AuthServices.deleteSession();
+		
 	}
 
 }
